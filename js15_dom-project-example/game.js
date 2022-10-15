be9f41677fff30abd8f78b6d16b166eb43703492
Mehmet Sorkulu <sorkulumehmet@gmail.com>
2022-10-15 17:25:07 +0300

    Again Btn

--- js15_dom-project-example/game.js
+++ js15_dom-project-example/game.js
@@ -7,13 +7,14 @@
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 const msg = document.querySelector(".msg");
-let playable = true;
+const body = document.querySelector("body");
 
 //? variables
 let score = 10;
 let topScore = 0;
 
 //* When the user presses the ChechBtn
+//! you won ==> background = green ==> if score > topScore topScore == score ==> secret number will be visiable
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   //? if there's no input ==> warning
@@ -22,30 +23,50 @@ document.querySelector(".check-btn").addEventListener("click", () => {
     //? if randomNumber == input.value
   } else if (randomNumber === guessInput) {
     msg.innerText = "Congrats You Win!";
+    document.querySelector("check-btn").disabled = true;
     document.querySelector("body").style.backgroundColor = "green";
     document.querySelector(".secret-number").innerText = randomNumber;
 
     if (score > topScore) {
       topScore = score;
       document.querySelector(".top-score").innerText = topScore;
-    } //? if randomNumber !== input.value look at the score ==>
-  } else if (randomNumber < guessInput) {
-    if (score > 0) {
-      score -= 1;
-      msg.innerText = "Decrease!";
+    }
+  }
+  //? if randomNumber !== input.value look at the score ==>
+  else if (randomNumber < guessInput) {
+    score -= 1;
+    document.querySelector(".score").innerText = score;
+    if (score <= 0) {
+      msg.innerText = "Decrease! ⏬⏬";
     } else {
       msg.innerText = "Sorry, You Lost! Play Again?";
-      playable = false;
+      document.querySelector("check-btn").disabled = true;
+    }
+  } else if (randomNumber > guessInput) {
+    if (score > 0) {
+      score -= 1;
+      document.querySelector(".score").innerText = score;
+      msg.innerText = "Increase! ⏫⏫";
+    } else {
+      msg.innerText = "Sorry, You Lost! Play Again?";
+      score = 0;
+      document.querySelector(".secret-number").innerText = randomNumber;
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".check-btn").disabled = true;
     }
   }
 });
 
-//! you won ==> background = green ==> if score > topScore topScore == score ==> secret number will be visiable
+//* When the user presses the againBtn reset the game
 
-//! if score > 0
-//!     score = score - 1
-//!     if random < input.value ===> decrease
-//!     else ===> increase
-//!else ==> Sorry you're lost.
-
-//* When the user presses the againBtn
+document.querySelector(".again-btn").addEventListener("click", () => {
+  score = 10;
+  const randomNumber = Math.round(Math.random() * 100);
+  console.log(randomNumber);
+  document.querySelector(".score").innerText = score;
+  document.querySelector(".check-btn").disabled = false;
+  document.querySelector(".secret-number").innerText = "?";
+  document.querySelector("body").classList.remove(".bg-success", ".bg-danger");
+  document.querySelector(".guess-input").value = ""
+  document.querySelector(".msg").innerText = "Starting.."
+});
