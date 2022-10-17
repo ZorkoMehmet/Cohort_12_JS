401b5b48786ca8a209bd120433943645a2539fdf
Mehmet Sorkulu <sorkulumehmet@gmail.com>
2022-10-17 20:27:02 +0300

    dom

--- js16_building-a-to-do-list/to-do-list.js
+++ js16_building-a-to-do-list/to-do-list.js
@@ -22,13 +22,19 @@ createListElement = (newTodo) => {
     const li = document.createElement("li")
     li.setAttribute("id", newTodo.id) // ALT: li.id = newTodo.id
     console.log(li)
+
     const okIcon = document.createElement("i")
     okIcon.setAttribute("class", "fas fa-check")
     li.appendChild(okIcon)
 
     todoUl.appendChild(li)
 
-    const newText = document.createElement("p")
-    // newText.setAttribute("")
-    li.appendChild(newText)
+    const p = document.createElement("p")
+    const pTextNode = document.createTextNode(newTodo.text)
+    p.appendChild(pTextNode)
+    li.appendChild(p)
+ 
+    const deleteIcon = document.createElement("i")
+    deleteIcon.setAttribute("class", "fas fa-trash")
+    li.appendChild(deleteIcon)
 }
