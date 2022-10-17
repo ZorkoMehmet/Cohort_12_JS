04027ef3eaae8aecac266591d0bb10b451633c4f
Mehmet Sorkulu <sorkulumehmet@gmail.com>
2022-10-17 20:36:41 +0300

    keydown and focus

--- js16_building-a-to-do-list/to-do-list.js
+++ js16_building-a-to-do-list/to-do-list.js
@@ -6,35 +6,46 @@ const total = document.getElementById("total");
 const completed = document.getElementById("completed");
 
 addBtn.addEventListener("click", () => {
-    if(todoInput.value.trim() === ""){
-        alert("Please enter what you wish to-do!")
-    } else {
-        const newTodo = {
-            id: new Date().getTime(),
-            completed: false,
-            text: todoInput.value
-        }
-        createListElement(newTodo)
-    }
+  if (todoInput.value.trim() === "") {
+    alert("Please enter what you wish to-do!");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+    createListElement(newTodo);
+    todoInput.value = "";
+  }
 });
 
 createListElement = (newTodo) => {
-    const li = document.createElement("li")
-    li.setAttribute("id", newTodo.id) // ALT: li.id = newTodo.id
-    console.log(li)
+  const li = document.createElement("li");
+  li.setAttribute("id", newTodo.id); // ALT: li.id = newTodo.id
+  console.log(li);
 
-    const okIcon = document.createElement("i")
-    okIcon.setAttribute("class", "fas fa-check")
-    li.appendChild(okIcon)
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
 
-    todoUl.appendChild(li)
+  todoUl.appendChild(li);
 
-    const p = document.createElement("p")
-    const pTextNode = document.createTextNode(newTodo.text)
-    p.appendChild(pTextNode)
-    li.appendChild(p)
- 
-    const deleteIcon = document.createElement("i")
-    deleteIcon.setAttribute("class", "fas fa-trash")
-    li.appendChild(deleteIcon)
+  const p = document.createElement("p");
+  const pTextNode = document.createTextNode(newTodo.text);
+  p.appendChild(pTextNode);
+  li.appendChild(p);
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(deleteIcon);
+};
+
+todoInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addBtn.click();
+  }
+});
+
+window.onload = function () {
+    todoInput.focus()
 }
