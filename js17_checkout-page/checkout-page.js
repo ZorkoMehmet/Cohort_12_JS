4057a25d595a69858c6d97553f814ba7967cd321
Mehmet Sorkulu <sorkulumehmet@gmail.com>
2022-10-20 13:34:28 +0300

    4

--- js17_checkout-page/checkout-page.js
+++ js17_checkout-page/checkout-page.js
@@ -3,7 +3,6 @@ const shippingPrice = 15;
 const shippingFreePrice = 300;
 
 window.addEventListener("load", () => {
-  calculateCartPrice();
   localStorage.setItem("taxRate", taxRate);
   localStorage.setItem("shippingPrice", shippingPrice);
   localStorage.setItem("shippingFreePrice", shippingFreePrice);
@@ -11,8 +10,31 @@ window.addEventListener("load", () => {
   //  sessionStorage.setItem("shippingPrice", shippingPrice);
   //  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
+
 const productsDiv = document.querySelector(".products");
 productsDiv.addEventListener("click", (event) => {
-  if (event.target.className == "fa-solid fa-minus") {
-
+  if (event.target.className == "fa-solid fa-plus") {
+    //! search for the previous sibling version of this
+    event.target.parentElement.querySelector(".quantity").innerText++;
   }
+  if (event.target.classList.contains("fa-minus")) {
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      confirm("Do you want to remove this item from your checkout list?"),
+        event.target.parentElement.parentElement.parentElement.remove();
+    }
+  }
+  if (event.target.className == "remove-product") {
+    event.target.parentElement.parentElement.parentElement.remove();
+  }
+});
+
+const calculateProductTotal = (btn) => {
+  const productsInfoDiv = btn.parentElement.parentElement
+
+  const productPrice = productsInfoDiv.querySelector("product-price strong").innerText
+  console.log(productPrice);
+};
+calculateProductTotal();
+const calculateSubTotal = () => {};
